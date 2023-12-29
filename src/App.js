6e4457a716f012e1ea567500752b6c359a6e0a89
alyssadva <alyssadiva26@gmail.com>
2023-12-29 15:47:04 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import TTS from "./TTS";
+
+function App() {
+  return (
+    <div className="App">
+      <TTS />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constans/puzzeData.js
@@ -0,0 +1,71 @@
+export const puzzleData = [
+  // Across
+  {
+    position: 1,
+    clue: "Paus & Buaya",
+    answer: "surabaya",
+    orientation: "across",
+    startCol: 1,
+    startRow: 4,
+  },
+
+  {
+    position: 4,
+    clue: "Hewan mitologi China",
+    answer: "naga",
+    orientation: "across",
+    startCol: 5,
+    startRow: 8,
+  },
+  {
+    position: 3,
+    clue: "Gear 5 Luffy adalah mythical Zoan model ...",
+    answer: "nika",
+    orientation: "across",
+    startCol: 2,
+    startRow: 7,
+  },
+
+  {
+    position: 2,
+    clue: "Sungai terpanjang di Indonesia",
+    answer: "kapuas",
+    orientation: "across",
+    startCol: 3,
+    startRow: 2,
+  },
+
+  // Down
+  {
+    position: 1,
+    clue: "Penyakit pernafasan",
+    answer: "asma",
+    orientation: "down",
+    startCol: 1,
+    startRow: 3,
+  },
+  {
+    position: 2,
+    clue: "Makanan tipis yang biasa terbuat dari singkong",
+    answer: "keripik",
+    orientation: "down",
+    startCol: 3,
+    startRow: 2,
+  },
+  {
+    position: 3,
+    clue: "Bagian dari pohon",
+    answer: "batang",
+    orientation: "down",
+    startCol: 5,
+    startRow: 4,
+  },
+  {
+    position: 4,
+    clue: "Tidak sedikit",
+    answer: "banyak",
+    orientation: "down",
+    startCol: 7,
+    startRow: 1,
+  },
+];
